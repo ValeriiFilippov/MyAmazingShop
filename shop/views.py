994a6c92b1e3b4c4d55f37d50b719efeb73c8859
Yatsenko Serhii <yatsenko.serj@gmail.com>
2021-08-13 20:44:04 +0300

    Add routing

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Category, Product
 
-# Create your views here.
+
+def index(request):
+    categories = Category.objects.all()
+
+    context = {
+        "categories": categories
+    }
+
+    return render(request, "index.html", context=context)
