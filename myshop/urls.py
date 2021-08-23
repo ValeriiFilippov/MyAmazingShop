84647a79ab7bf98469413e2c4f86be2b4c8e0c9a
Yatsenko Serhii <yatsenko.serj@gmail.com>
2021-08-23 04:18:24 +0300

    Add styles

--- myshop/urls.py
+++ myshop/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 
 urlpatterns = [
-    path("shop/", include("shop.urls")),
     path("admin/", admin.site.urls),
+    path("shop/", include("shop.urls")),
+    path('', RedirectView.as_view(url='/shop/', permanent=True)),
 ]
